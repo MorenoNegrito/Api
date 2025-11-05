77f1162fa385b0f50d2d9e1a6278136dbead8a17
MorenoNegrito <negrito.pankeke@gmailcom>
2025-11-05 20:50:57 -0300

    Primer commit del proyecto Spring Boot

---
+++ api-desarrolladores/src/test/java/com/example/api_desarrolladores/ApiDesarrolladoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_desarrolladores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDesarrolladoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
